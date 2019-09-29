33871fc39e8d92102858ff1cb6e570555c537a82
HaveAGitGat <43864057+HaveAGitGat@users.noreply.github.com>
2019-09-29 15:06:18 +0100

    Init update

--- imports/ui/tab_Settings.jsx
+++ imports/ui/tab_Settings.jsx
@@ -8,6 +8,8 @@ import { SettingsDB } from '../api/tasks.js';
 
 import Folder from '../ui/tab_Settings_Folder.jsx';
 import ClipLoader from 'react-spinners/ClipLoader';
+import { Button } from 'react-bootstrap';
+import Modal from "reactjs-popup";
 
 
 
@@ -42,9 +44,9 @@ class App extends Component {
   addFolder(){
 
     SettingsDB.insert({
-      folder:"C:/MySourceFolder",
+      folder:"Path to your media files. Input a base folder and the folder browser below will show subfolders to navigate.",
       folderValid:true,
-      cache:"C:/MyTranscodeCache",
+      cache:"Path to your transcode cache. Input a base folder and the folder browser below will show subfolders to navigate.",
       cacheValid:true,
       container:".mkv",
       containerFilter:"mkv,mp4,mov,m4v,mpg,mpeg,avi,flv,webm,wmv,vob,evo,iso,",
@@ -54,6 +56,7 @@ class App extends Component {
       scanFound:0,
       expanded:true,
       pluginID:'',
+      pluginIDs:[],
       pluginValid:false,
       pluginCommunity:true,
       handbrake:true,
@@ -177,13 +180,13 @@ class App extends Component {
 
   componentDidMount = () => {
 
-    render(<ClipLoader
+    render(<center><ClipLoader
 
       sizeUnit={"px"}
       size={25}
       color={'#000000'}
       loading={true}
-  />, document.getElementById('status'));
+  /></center>, document.getElementById('status'));
 
 
     Meteor.subscribe('SettingsDB', function(){
@@ -194,7 +197,7 @@ class App extends Component {
       if(res.length == 0){
   
   
-        render(<p>No libraries</p>, document.getElementById('status'));
+        render(<center><p>No libraries</p></center>, document.getElementById('status'));
         
   
     
@@ -242,11 +245,81 @@ class App extends Component {
 
 
       <div className="containerGeneral">
+        <center>
       <header>
           <h1>Libraries</h1>
       </header>
-        <input type="button" className="addFolderButton" onClick={this.addFolder} value="Library +"/>
-        <input type="button" className="cancelAllWorkersButton" onClick={this.clearFiles} value="Delete all libraries"/>
+      </center>
+      
+      <center>
+
+        <Button variant="outline-dark"  onClick={this.addFolder} >Library +</Button>
+        <Button variant="outline-danger"  onClick={this.clearFiles} >Delete all libraries</Button>
+        <Modal
+          trigger={<Button variant="outline-dark" >i</Button>}
+          modal
+          closeOnDocumentClick
+        >
+          <div className="frame">
+            <div className="scroll">
+
+
+            <p>Tdarr is designed to automatically manage transcoding and health checking of your media libraries. If you've used HBBatchBeast, a lot of the settings will look familiar.</p>
+
+
+<p>When you're first getting started, add a new library on the 'Libraries' tab and specify a source folder, select the container types you'd like to scan for and then click then click 'Scan (Fresh)'. This will do a fresh scan of the library and extract meta-data from files which fit your specified container settings. The data will then be saved in the database.</p>
+
+<p>Provide a base folder path and the folder browser will show subfolders which you can navigate to using the buttons.</p>
+
+<img src="https://i.imgur.com/vyp7fle.png"/>
+
+<p>'Scan (Find new)' does 2 things - it removes files from the database which don't exist anymore, and adds newly detected files. Turn on 'Folder watch' to automate this process.</p>
+<p>All new files will be added to both the transcode queues and the health check queues. If you're not interested in using one or the other, then just make sure not to fire up any workers for that respective queue.</p>
+
+<p></p>
+<p></p>
+<p></p>
+<p>In the library transcode settings, you can either use plugins or manually set either video file or audio file transcode settings.</p>
+
+<p>You can stack plugins but this will increase the number of transcode/remux passes on a file. It's best to use/create a plugin which can do what you need all at once.</p>
+<img src="https://i.imgur.com/yGmb9Rn.png"/>
+
+
+<p>See the plugins tab for plugins info on creating a plugin.</p>
+
+<p></p>
+<p></p>
+<p></p>
+<p>In the manual settings, if using FFmpeg, you need to separate the input and output parameters with a comma. Such as:  '-r 1,-r 24'. Here are some HandBrake preset examples:</p>
+<p>-e x264 -q 20 -B</p>
+<p>-Z "Very Fast 1080p30"</p>
+<p>-Z "Very Fast 480p30"</p>
+<p>--preset-import-file "C:\Users\HaveAGitGat\Desktop\testpreset.json" -Z "My Preset"</p>
+
+
+<p>You can find learn more about HandBrake presets here:</p>
+
+<p>https://handbrake.fr/docs/en/latest/technical/official-presets.html</p>
+
+<p>If you're having trouble with custom presets, it may be due to a known bug with the HandBrakeCLI (will be fixed in next HandBrakeCLI release). Please see this for a temporary solution:  https://github.com/HandBrake/HandBrake/issues/2047</p>
+<p></p>
+<p>Regarding the transcode filter settings, these are applied when the items are being processed in the transcode queue. If files do not meet the transcode requirements then they will be passed.</p>
+
+<p></p>
+
+<p>Quick health checks are done using HandBrake's --scan feature which checks file header for errors. Thorough health checks are done using FFmpeg to go through all frames in a video file.</p>
+
+<p></p>
+
+<p>Watch out for the schedule settings. If scheduled blocks are ticked, and worker limits are set at the top of the 'Tdarr' tab, then workers will be fired up even if you keep closing them down. It's also important to note that workers won't process items for the respective library outside the selected time blocks.</p>
+
+
+            
+
+            </div>
+          </div>
+        </Modal>
+        </center>
         <p></p>
         <p></p>
 
