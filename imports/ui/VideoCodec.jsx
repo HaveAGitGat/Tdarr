a2d3d11eca6cf0f1954e619597bb74a13dc83c5e
HaveAGitGat <43864057+HaveAGitGat@users.noreply.github.com>
2019-09-29 15:03:07 +0100

    Re-format from list to table item

--- imports/ui/VideoCodec.jsx
+++ imports/ui/VideoCodec.jsx
@@ -1,56 +1,68 @@
 import React, { Component } from 'react';
+import Checkbox from '@material-ui/core/Checkbox';
 
 
 export default class VideoCodec extends Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.toggleChecked = this.toggleChecked.bind(this);
-        this.deleteThisCodec = this.deleteThisCodec.bind(this);
-    
-    
-      }
-
-    toggleChecked(event) {
+    this.toggleChecked = this.toggleChecked.bind(this);
+    this.deleteThisCodec = this.deleteThisCodec.bind(this);
 
 
-            Meteor.call('updateVideoCodecExclude',this.props.DB_id, this.props.videocodec.codec,event.target.checked,function (error, result) { });
-
-
-    }
-
-    deleteThisCodec(){
-
-
-        Meteor.call('removeVideoCodecExclude',this.props.DB_id, this.props.videocodec.codec,event.target.checked,function (error, result) { });
-
-    }
-
-
-
-    render() {
-      return (
-        <li key={this.props.key} >
-         <input type="checkbox" checked={!!this.props.videocodec.checked} onChange={this.toggleChecked} />
-        {this.props.videocodec.codec}
-        
-
-      <div  style={noBreak}>
-         <button className="deleteCodecButton" onClick={this.deleteThisCodec.bind(this)}>
-        
-          &times;
-        </button>
-        </div>
-
-        
-        
-        </li>
-      );
-    }
   }
 
-  
+  toggleChecked(event) {
+
+
+    Meteor.call('updateVideoCodecExclude', this.props.DB_id, this.props.videocodec.codec, event.target.checked, function (error, result) { });
+
+
+  }
+
+  deleteThisCodec() {
+
+
+    Meteor.call('removeVideoCodecExclude', this.props.DB_id, this.props.videocodec.codec, event.target.checked, function (error, result) { });
+
+  }
+
+
+
+  render() {
+
+    return (
+
+
+      <tr>
+        <td>
+
+          <Checkbox checked={!!this.props.videocodec.checked} onChange={this.toggleChecked} />
+        </td>
+
+        <td>
+          {this.props.videocodec.codec}
+        </td>
+
+
+
+        <td>
+          <button className="deleteCodecButton" onClick={this.deleteThisCodec.bind(this)}>
+
+            &times;
+        </button>
+        </td>
+
+
+
+
+      </tr>
+    );
+  }
+}
+
+
 var noBreak = {
   display: 'inline-block',
 }
