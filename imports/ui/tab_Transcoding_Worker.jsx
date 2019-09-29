c4d56e134b8cd8b6f6ca05b24ad0a07d4e1eccc7
HaveAGitGat <43864057+HaveAGitGat@users.noreply.github.com>
2019-09-29 15:07:33 +0100

    Hide percentage if no total frame count

--- imports/ui/tab_Transcoding_Worker.jsx
+++ imports/ui/tab_Transcoding_Worker.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
-import { ProgressBar } from 'react-bootstrap';
+import { Progress } from 'react-sweet-progress';
+import "react-sweet-progress/lib/style.css";
+
+import { Button } from 'react-bootstrap';
 
 
 import ToggleButton from 'react-toggle-button'
@@ -27,101 +30,130 @@ export default class Worker extends Component {
 
     return (
       <div className="workerContainer">
-        <div className={this.props.worker.modeType == 'transcode' ? "borderStyleTranscode": this.props.worker.modeType == 'healthcheck' ? "borderStyleHealthCheck": "borderStyleGeneral"}>
-
-
-  
-        <div className="workerContainerItems">
-
-
-      <p></p>
-
-        <div className="workerItemsGrid">
-
-        <div className="grid-item"  style={ButtonStyle} >
-          <div style={ButtonStyle} className="toggleWorkerButton">
-            <ToggleButton
-
-              value={!this.props.worker.idle || false}
-              onToggle={() => {
-                Meteor.call('upsertWorkers', this.props.worker._id,{
-                  idle: !this.props.worker.idle,
-                }, function (error, result) { });
-              }
-
-              }
-            />
-          </div>
-          </div>
-
-          <div className="grid-item"  style={ButtonStyle}>
-          <div style={ButtonStyle}>
-      
-          {this.props.worker.mode == 'transcode' ? "Transcode": this.props.worker.mode == 'healthcheck' ? "Heath check": "General("+workerModes[this.props.worker.modeType]+")"}
-
-          </div>
-          </div>
+        <div className={this.props.worker.modeType == 'transcode' ? "borderStyleTranscode" : this.props.worker.modeType == 'healthcheck' ? "borderStyleHealthCheck" : "borderStyleGeneral"}>
 
 
 
-          <div className="grid-item" style={ButtonStyle}>
-          <div style={ButtonStyle}>
-
-  
-<input type="button" className="cancelWorkerButton"  style={ButtonStyle} onClick={() => {
+          <div className="workerContainerItems">
 
 
-  Meteor.call('cancelWorkerItem', this.props.worker._id, function (error, result) { })
+            <p></p>
 
+            <div className="workerItemsGrid">
 
-}} value="Cancel item"/>
- </div>
- </div>
+              <div className="grid-item" style={ButtonStyle} >
+                <div style={ButtonStyle} className="toggleWorkerButton">
+                  <ToggleButton
 
- 
- <div className="grid-item" style={ButtonStyle}>
-          <div style={ButtonStyle}>
+                    value={!this.props.worker.idle || false}
+                    onToggle={() => {
+                      Meteor.call('upsertWorkers', this.props.worker._id, {
+                        idle: !this.props.worker.idle,
+                      }, function (error, result) { });
+                    }
 
-  
-          <input type="button" className="cancelWorkerButton" style={ButtonStyle} onClick={() => {
+                    }
+                  />
+                </div>
+              </div>
 
+              <div className="grid-item" style={ButtonStyle}>
+                <div style={ButtonStyle}>
 
-            Meteor.call('killWorker', this.props.worker._id, this.props.worker.file, this.props.worker.mode, function (error, result) { })
+                  {this.props.worker.mode == 'transcode' ? "Transcode" : this.props.worker.mode == 'healthcheck' ? "Heath check" : "General(" + workerModes[this.props.worker.modeType] + ")"}
 
-
-          }} value="X"/>
-
-</div>
-         
-           </div>
-       </div>
-
-        <p>
-        {path.basename(this.props.worker.file)}
-        </p>
+                </div>
+              </div>
 
 
 
+              <div className="grid-item" style={ButtonStyle}>
+                <div style={ButtonStyle}>
 
-<style type="text/css">
-{`
-.progressbar-custom {
-  background-color: purple;
-  color: white;
-}
+
+                  <Button variant="outline-danger" style={ButtonStyle} onClick={() => {
+
+
+                    Meteor.call('cancelWorkerItem', this.props.worker._id, function (error, result) { })
+
+
+                  }} >Cancel item</Button>
+                </div>
+              </div>
+
+
+              <div className="grid-item" style={ButtonStyle}>
+                <div style={ButtonStyle}>
+
+
+                  <Button variant="outline-danger" style={ButtonStyle} onClick={() => {
+
+
+                    Meteor.call('killWorker', this.props.worker._id, this.props.worker.file, this.props.worker.mode, function (error, result) { })
+
+
+                  }} >X</Button>
+
+                </div>
+
+              </div>
+            </div>
+
+            <p>
+              {path.basename(this.props.worker.file)}
+            </p>
+
+
+
+
+            <style type="text/css">
+              {`
+
 `}
-  </style>
+
+
+            </style>
 
 
 
-        <ProgressBar now={this.props.worker.percentage}  label={this.props.worker.percentage+"%"} variant="custom" />
 
-<div className={this.props.worker.percentage > 100 ? '' : 'hidden'}>
+            <div className={this.props.worker.percentage <= 100 ? '' : 'hidden'}>
+              <Progress percent={parseInt(this.props.worker.percentage)} status="default"
 
-        No total frame count. Frame: {this.props.worker.percentage/100 }
+                theme={
+                  {
+                    default: {
+                      symbol: this.props.worker.percentage + '%',
+                      trailColor: 'white',
+                      color: 'black'
+                    },
+                  }
+                }
+              />
 
-        </div>
-        </div>
+            </div>
+
+
+            <div className={this.props.worker.percentage > 100 ? '' : 'hidden'}>
+
+              <Progress percent={100} status="default"
+
+                theme={
+                  {
+                    default: {
+                      symbol: '-',
+                      trailColor: 'white',
+                      color: 'black'
+                    },
+                  }
+                }
+              />
+              <center >
+                No total frame count. Frame: {this.props.worker.percentage / 100}
+              </center>
+
+            </div>
+          </div>
         </div>
       </div>
     );
@@ -129,6 +161,7 @@ export default class Worker extends Component {
 }
 
 var workerModes = {
-  healthcheck:"Health",
-  transcode:"Transcode"
+  healthcheck: "Health",
+  transcode: "Transcode"
 }
+
