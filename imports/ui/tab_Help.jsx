a807809cc81ba5494cdcd1ae55454c63ea4f7ca3
HaveAGitGat <43864057+HaveAGitGat@users.noreply.github.com>
2019-10-04 14:38:37 +0100

    Icon implementation

--- imports/ui/tab_Help.jsx
+++ imports/ui/tab_Help.jsx
@@ -27,22 +27,48 @@ export default class App extends Component {
 
 
       <div className="containerGeneral">
-         <center>
-      <header>
-          <h1>Help</h1>
-      </header>
-       </center>
+        <center>
+          <header>
+            <h1>Help</h1>
+          </header>
+        </center>
+
+        <br></br>
+        <br></br>
+
+        <center>
 
 
-       <center>
-          <p>Reddit:https://www.reddit.com/r/Tdarr/</p>
-          </center>
-          <center>
-          <p>Discord:https://discord.gg/GF8X8cq</p>
-          </center>
+ 
 
 
-        </div>
+          <div className="iconContainer">
+            <a href={"https://github.com/HaveAGitGat/Tdarr"}><img src={"/images/icons/G.png"} alt='icon' height="50" width="50" /></a>
+          </div>
+
+          <br></br>
+          <br></br>
+
+
+          <div className="iconContainer">
+            <a href={"https://www.reddit.com/r/Tdarr"}><img src={"/images/icons/R.png"} alt='icon' height="50" width="50" /></a>
+          </div>
+
+          <br></br>
+          <br></br>
+
+
+
+          <div className="iconContainer">
+            <a href={"https://discord.gg/GF8X8cq"}><img src={"/images/icons/D.svg"} alt='icon' height="60" width="60" /></a>
+          </div>
+
+
+        </center>
+
+
+
+      </div>
 
     );
   }
