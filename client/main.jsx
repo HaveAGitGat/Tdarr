50adc8fd6b01286f425c4932ddd23cf1923f0567
Jono Cairns <jonocairns@gmail.com>
2019-11-02 15:38:01 +1300

    Refactor navigation

--- client/main.jsx
+++ client/main.jsx
@@ -3,15 +3,7 @@ import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import App from '/imports/ui/App'
 
-
-
-
-
-
-
-
 Meteor.startup(() => {
-
   render(<App />, document.getElementById('react-target'));
 });
 
