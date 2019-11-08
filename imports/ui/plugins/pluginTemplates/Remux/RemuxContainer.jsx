0bcf0c10f1c07216d89ebe52079c08f9301148b8
HaveAGitGat <43864057+HaveAGitGat@users.noreply.github.com>
2019-11-08 05:07:55 +0000

    Plugin creator - Remux container

---
+++ imports/ui/plugins/pluginTemplates/Remux/RemuxContainer.jsx
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Button} from 'react-bootstrap';
+import ReactDOM from 'react-dom';
+import Checkbox from '@material-ui/core/Checkbox';
+
+
+
+
+
+var ButtonStyle = {
+  display: 'inline-block',
+}
+
+
+
+ class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      handBrakeMode:true,
+      FFmpegMode:false
+  
+    };
+
+
+  }
+
+
+    createPlugin = () => {
+
+
+
+      var container = ReactDOM.findDOMNode(this.refs.container).value
+
+      if(container.charAt(0) !== "."){
+
+        container =  "."+ container
+
+      }
+   
+
+Meteor.call('createPluginRemuxContainer', container, function (error, result) { })
+
+
+    }
+
+
+
+  render() {
+
+
+
+    return (
+
+
+
+
+
+      <div >
+
+<br/>
+<br/>
+<br/>
+
+<center><p>Remux container</p> </center>
+
+
+
+          <p>Make sure files are in:</p>
+       <br/>
+
+
+       <input type="text" className="folderPaths" ref="container"  defaultValue={"mkv"}></input>
+
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+
+
+        <center>
+
+       <Button variant="outline-light" onClick={this.createPlugin}  >Create Plugin</Button>
+
+       </center>
+
+       <br/>
+       <br/>
+       <br/>
+
+
+
+      </div>
+
+    );
+  }
+}
+
+export default withTracker(() => {
+
+  
+
+
+return {
+ 
+
+
+};
+})(App);
+
+
