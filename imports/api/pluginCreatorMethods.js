78c05d8b8716bd96c14755735e35cebf7adf6936
HaveAGitGat <43864057+HaveAGitGat@users.noreply.github.com>
2019-11-30 23:04:29 +0000

    print paths required in plugin creator

--- imports/api/pluginCreatorMethods.js
+++ imports/api/pluginCreatorMethods.js
@@ -21,7 +21,15 @@ Meteor.methods({
 
     var text = `
 
-    const importFresh = require('import-fresh');
+    var fs = require('fs');
+    var path = require('path');
+    if (fs.existsSync(path.join(process.cwd() + '/npm'))) {
+    var rootModules = path.join(process.cwd() + '/npm/node_modules/')
+    } else{
+    var rootModules = ''
+    }
+   
+    const importFresh = require(rootModules+'import-fresh');
     const library = importFresh('../methods/library.js')
       
       function details() {
