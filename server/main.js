6caa21f055e15ddd465c0ee458397103f26860f0
HaveAGitGat <43864057+HaveAGitGat@users.noreply.github.com>
2020-04-24 22:45:03 +0100

    Refactor folder init

--- server/main.js
+++ server/main.js
@@ -138,70 +138,14 @@ console.log("Tdarr documents folder:" + homePath)
 console.log("Checking directories")
 
 
-const isDocker = require('is-docker');
-
-if (isDocker()) {
-  console.log('Running inside a Docker container');
-  if (!fs.existsSync("/temp")) {
-    fs.mkdirSync("/temp");
-
-  }
-} else {
-  console.log('Not running inside a Docker container');
-
-}
 
 
 
+//Create Tdarr documents folder structure if not exist
+import initFolders from './initFolders.js';
+initFolders(homePath)
 
 
-
-if (!fs.existsSync(homePath + "")) {
-  fs.mkdirSync(homePath + "");
-
-}
-
-if (!fs.existsSync(homePath + "/Tdarr")) {
-  fs.mkdirSync(homePath + "/Tdarr");
-
-}
-
-
-
-if (!fs.existsSync(homePath + "/Tdarr/Plugins")) {
-  fs.mkdirSync(homePath + "/Tdarr/Plugins");
-
-}
-
-if (!fs.existsSync(homePath + "/Tdarr/Data")) {
-  fs.mkdirSync(homePath + "/Tdarr/Data");
-
-}
-
-if (!fs.existsSync(homePath + "/Tdarr/Plugins/Community")) {
-  fs.mkdirSync(homePath + "/Tdarr/Plugins/Community");
-
-}
-
-if (!fs.existsSync(homePath + "/Tdarr/Plugins/Local")) {
-  fs.mkdirSync(homePath + "/Tdarr/Plugins/Local");
-
-}
-
-if (!fs.existsSync(homePath + "/Tdarr/Samples")) {
-  fs.mkdirSync(homePath + "/Tdarr/Samples");
-
-}
-
-if (!fs.existsSync(homePath + "/Tdarr/Backups")) {
-  fs.mkdirSync(homePath + "/Tdarr/Backups");
-
-}
-
-if (!fs.existsSync(homePath + "/Tdarr/Logs")) {
-  fs.mkdirSync(homePath + "/Tdarr/Logs");
-
-}
 
 GlobalSettingsDB.upsert(
   "globalsettings",
