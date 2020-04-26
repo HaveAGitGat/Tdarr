455cc1eaae3c3beb2d20dc556aaa51bf59b9558f
HaveAGitGat <43864057+HaveAGitGat@users.noreply.github.com>
2020-04-26 02:55:27 +0100

    Move CLI funcs - prevent undefined on start

--- server/main.js
+++ server/main.js
@@ -1509,65 +1509,6 @@ function main() {
     setTimeout(Meteor.bindEnvironment(runScheduledManualScan), 3600000);
   }
 
-  if (fs.existsSync(path.join(process.cwd(), "/npm"))) {
-    var handBrakeCLIPath = path.join(process.cwd(), '/assets/app/HandBrakeCLI.exe')
-    var ffmpegPathLinux345 = path.join(process.cwd(), '/assets/app/ffmpeg/ffmpeg345/ffmpeg')
-    var ffmpegPathLinux42 = path.join(process.cwd(), '/assets/app/ffmpeg/ffmpeg42/ffmpeg')
-  } else {
-    var handBrakeCLIPath = path.join(process.cwd(), '/private/HandBrakeCLI.exe')
-    var ffmpegPathLinux345 = path.join(process.cwd(), '/private/ffmpeg/ffmpeg345/ffmpeg')
-    var ffmpegPathLinux42 = path.join(process.cwd(), '/private/ffmpeg/ffmpeg42/ffmpeg')
-  }
-
-  var ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
-  ffmpegPathLinux345 = ffmpegPathLinux345.replace(/'/g, '\'\"\'\"\'');
-  ffmpegPathLinux42 = ffmpegPathLinux42.replace(/'/g, '\'\"\'\"\'');
-
-  function getHandBrakePath() {
-    var path
-
-    if (process.platform == 'win32') {
-      path = handBrakeCLIPath
-    }
-
-    if (process.platform == 'linux') {
-      path = "HandBrakeCLI"
-    }
-
-    if (process.platform == 'darwin') {
-      path = "/usr/local/bin/HandBrakeCLI"
-    }
-
-    return path
-  }
-
-  function getFFmpegPath() {
-    var path
-
-    if (process.platform == 'win32') {
-      path = ffmpegPath
-    }
-
-    if (process.platform == 'linux') {
-      path = ffmpegPathLinux42
-    }
-
-    if (process.platform == 'darwin') {
-      path = ffmpegPath
-    }
-
-    var ffmpegNVENCBinary = (GlobalSettingsDB.find({}, {}).fetch())[0].ffmpegNVENCBinary
-
-    if (ffmpegNVENCBinary == true) {
-      if (process.platform == 'linux') {
-        path = ffmpegPathLinux345
-      }
-    }
-
-    return path
-  }
-
-
   var shell = require('shelljs');
   scheduledPluginUpdate()
 
