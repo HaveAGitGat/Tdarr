3ce536f0d0cb81c8f4886c053ce08cf67e8d5a76
HaveAGitGat <43864057+HaveAGitGat@users.noreply.github.com>
2020-04-28 00:44:46 +0100

    Modularise proc priority

--- server/main.js
+++ server/main.js
@@ -17,6 +17,7 @@ import "./crudFileDB.js";
 import "./dateFuncs.js";
 import "./pluginFuncs.js";
 
+import procPriority from "./procPriority.js";
 //Globals
 const shortid = require("shortid");
 const path = require("path");
@@ -2765,55 +2766,8 @@ function main() {
     return data;
   }
 
-  //set cpu priority low
-  setProcessPriority();
-
-  function setProcessPriority() {
-    try {
-      if (process.platform == "win32") {
-        var workerCommandFFmpeg =
-          'wmic process where name="ffmpeg.exe" CALL setpriority "Low"';
-        var workerCommandHandBrake =
-          'wmic process where name="HandBrakeCLI.exe" CALL setpriority "Low"';
-      }
-
-      if (process.platform == "linux") {
-        var workerCommandFFmpeg =
-          "for p in $(pgrep ^ffmpeg$); do renice -n 20 -p $p; done";
-        var workerCommandHandBrake =
-          "for p in $(pgrep ^HandBrakeCLI$); do renice -n 20 -p $p; done";
-      }
-
-      if (process.platform == "darwin") {
-        var workerCommandFFmpeg =
-          "for p in $(pgrep ^ffmpeg$); do renice -n 20 -p $p; done";
-        var workerCommandHandBrake =
-          "for p in $(pgrep ^HandBrakeCLI$); do renice -n 20 -p $p; done";
-      }
-
-      var shell = require("shelljs");
-      var globalSettings = GlobalSettingsDB.find({}, {}).fetch();
-
-      //logger.info(globalSettings)
-      if (globalSettings[0].lowCPUPriority == true) {
-        shellWorker = shell.exec(workerCommandFFmpeg, function (
-          code,
-          stdout,
-          stderr,
-          stdin
-        ) { });
-        shellWorker = shell.exec(workerCommandHandBrake, function (
-          code,
-          stdout,
-          stderr,
-          stdin
-        ) { });
-      }
-    } catch (err) {
-      logger.error(err.stack);
-    }
-    setTimeout(Meteor.bindEnvironment(setProcessPriority), 10000);
-  }
+  //loop, set cpu priority low
+  procPriority.setProcessPriority();
 
   var workerStatus = {};
   workerUpdateCheck();
