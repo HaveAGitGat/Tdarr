d8be189b5586ea6882ca5f3eb406cba1b5c5c301
HaveAGitGat <43864057+HaveAGitGat@users.noreply.github.com>
2020-04-28 00:53:00 +0100

    Modularise cache cleaner

--- server/main.js
+++ server/main.js
@@ -19,6 +19,8 @@ import "./pluginFuncs.js";
 
 import procPriority from "./procPriority.js";
 import scheduledSimpleScan from "./scheduledSimpleScan.js";
+import cacheClean from "./cacheClean.js";
+
 
 //Globals
 const shortid = require("shortid");
@@ -831,73 +833,9 @@ function main() {
     setTimeout(Meteor.bindEnvironment(scheduledPluginUpdate), 3600000);
   }
 
-  scheduledCacheClean();
-  function scheduledCacheClean() {
-    try {
-      var settings = SettingsDB.find({}, { sort: { createdAt: 1 } }).fetch();
+  //Check transcode cache for files which can be deleted (on startup)
+  cacheClean(filesBeingProcessed);
 
-      for (var i = 0; i < settings.length; i++) {
-        try {
-          traverseDir(settings[i].cache);
-        } catch (err) {
-          logger.error(err.stack);
-        }
-      }
-
-      function traverseDir(inputPathStem) {
-        try {
-          fs.readdirSync(inputPathStem).forEach((file) => {
-            var fullPath = path.join(inputPathStem, file).replace(/\\/g, "/");
-            function isBeingProcessed(filePath) {
-              for (var i = 0; i < filesBeingProcessed.length; i++) {
-                var base = filesBeingProcessed[i].split("/");
-                base = base[base.length - 1];
-                base = base.split(".");
-                base = base[base.length - 2];
-                if (filePath.includes(base)) {
-                  return true;
-                }
-              }
-              return false;
-            }
-
-            try {
-              if (
-                isBeingProcessed(fullPath) == false &&
-                fullPath.includes("TdarrCacheFile")
-              ) {
-                logger.info("Removing:" + fullPath);
-
-                try {
-                  rimraf.sync(fullPath);
-                } catch (err) {
-                  logger.error(err.stack);
-                  try {
-                    traverseDir(fullPath);
-                  } catch (err) {
-                    logger.error(err.stack);
-                  }
-                }
-              } else if (!fullPath.includes("TdarrCacheFile")) {
-                logger.info(
-                  "File not Tdarr cache file, won't remove:" + fullPath
-                );
-              } else {
-                logger.warn("Cache file in use, won't remove:" + fullPath);
-              }
-            } catch (err) {
-              logger.error(err.stack);
-            }
-          });
-        } catch (err) {
-          logger.error(err.stack);
-        }
-      }
-    } catch (err) {
-      logger.error(err.stack);
-    }
-    // setTimeout(Meteor.bindEnvironment(scheduledCacheClean), 10000);
-  }
 
   function findWorker(w_id) {
     if (w_id) {
