ca8535f1ef53ddac028e16de10d0dc05dff3c74f
HaveAGitGat <43864057+HaveAGitGat@users.noreply.github.com>
2025-10-20 17:41:20 +0100

    chore: use port 5266

--- demo_socket/server.js
+++ demo_socket/server.js
@@ -65,7 +65,7 @@ io.engine.on('connection_error', (err) => {
 });
 
 // Start the server
-const PORT = 8266;
+const PORT = 5266;
 const HOST = 'localhost';  // Bind to all interfaces instead of just localhost
 
 fastify.listen({ port: PORT, host: HOST }, (err, address) => {
